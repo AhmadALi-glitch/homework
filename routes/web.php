f03093f8cb9e9a875a4ab64f8abedc6024fcc72b
Ahmadali_glitch <newTownElectroo@gmail.com>
2023-03-02 16:50:59 +0300

    Clean The Code

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::match('post', '/register', [UserController::class, 'register'])->name('register');
-Route::match('post', '/login', [UserController::class, 'login'])->name('login');
+// Each Route Redirect The Requst To it's Related Method In The Controllers
+
+Route::post('/register', [UserController::class, 'register'])->name('register');
+Route::post('/login', [UserController::class, 'login'])->name('login');
 
 Route::get('show-product/{id}', [ProductsController::class, 'show'])->name('show-product');
 Route::get('get-products', [ProductsController::class, 'index'])->name('get-products');
 
+// Apply The Middleware On Those Routes  (That Allows Just The User That Has The Token)
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('store-product', [ProductsController::class, 'store'])->name('store-product');
